ba8c4ac2206122dc35f77c6b120f599ec8a8d8d4
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2025-05-02 21:51:56 -0700

    asset update

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,23 +1,23 @@
 <template>
   <q-page class="">
     <q-img
-      src="/assets/sunheartinwords768X1024.jpg"
+      src="src/assets/sunheartinwords768X1024.jpg"
     />
     <q-img
-      src="/assets/sunheartrainbowlogo.jpg"
+      src="src/assets/sunheartrainbowlogo.jpg"
     />
     <div class="row">
       <q-img
         class="col"
-        src="/assets/davidee2.jpg"
+        src="src/assets/davidee2.jpg"
       />
       <q-img
         class="col"
-        src="/assets/omni2.jpg"
+        src="src/assets/omni2.jpg"
       />
       <q-img
         class="col"
-        src="/assets/dawn.jpg"
+        src="src/assets/dawn.jpg"
       />
     </div>
     <h5>Tantric Devotional New Age Acid Folk Feminist Goddess Grunge</h5>
