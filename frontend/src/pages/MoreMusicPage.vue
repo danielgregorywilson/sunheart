eca3230aaca2f154df3def95e6a9c4190d91662d
david <sundance@efn.org>
2025-07-29 12:24:01 -0700

    trying to tweak the background - how do I get sunheartinwords image onto this page??

--- frontend/src/pages/MoreMusicPage.vue
+++ frontend/src/pages/MoreMusicPage.vue
@@ -1,8 +1,13 @@
 <template>
   <q-page class="">
-    <div class="text-h3">Our Friends Make Lovely Sounds</div>
-
-    <div><a href="https://eostar.net/">Eostar Kamala</a> Eostar wrote Only Forgiveness and is featured on backup vocals on Bless The Lessons and In Our Time</div>
+    <div class="row" style="max-width: 1000px; margin: auto;">
+        <q-img
+          id="logo"
+          src="/images/sunheartrainbowlogo.jpg"
+        />
+      </div>
+      <div class="text-h3">Our Friends Make Lovely Sounds</div>
+      <div><a href="https://eostar.net/">Eostar Kamala</a> Eostar wrote Only Forgiveness and is featured on backup vocals on Bless The Lessons and In Our Time</div>
     <div><a href="https://tidal.com/browse/artist/5895786">Ananda Yogiji</a>- Ananda is an original Sunheart booster. We love you!</div>
     <div><a href="http://www.palomadevi.com/">Paloma Devi</a> - angel voice</div>
     <div>Gurumukh Mark Harris - Mukh has been an inspiration for members of Sunheart since the 1970s!</div>
@@ -16,12 +21,14 @@
 </script>
 
 <style scoped>
+
 div {
   color: white;
   text-align: center;
   font-weight: 900;
   margin-top: 10px;
   margin-bottom: 10px;
+  /* background-image: url('/images/sunheartinwords768X1024.jpg'); */
 }
 
 </style>
