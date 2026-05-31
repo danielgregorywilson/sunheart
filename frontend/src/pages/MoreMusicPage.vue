df0880c73ebccb97ad34d67feb98027cfd653315
david <sundance@efn.org>
2026-05-31 16:31:09 -0700

    I updated the links to our music friends, adding a current link for Deva Priyo, and for Gurumukh, and changing Paloma Devi's link to https, on the presumption that here site is indeed https

--- frontend/src/pages/MoreMusicPage.vue
+++ frontend/src/pages/MoreMusicPage.vue
@@ -9,10 +9,9 @@
       <div class="text-h2">Our Friends Make Lovely Sounds</div>
       <div><a href="https://eostar.net/">Eostar Kamala</a> Eostar wrote Only Forgiveness and is featured on backup vocals on Bless The Lessons and In Our Time</div>
     <div><a href="https://tidal.com/browse/artist/5895786">Ananda Yogiji</a>- Ananda is an original Sunheart booster. We love you!</div>
-    <div><a href="http://www.palomadevi.com/">Paloma Devi</a> - angel voice</div>
-    <div>Gurumukh Mark Harris - Mukh has been an inspiration for members of Sunheart since the 1970s!</div>
-    <div>Deva Priyo's Lost at Last/One at Last - Priyo is featured on Sunheart's Nice, Unicorn Song and Wanderers Three</div>
-    <div>Deva Priyo Nicola Notaro's Gypsy Moon and solo work</div>
+    <div><a href="https://www.palomadevi.com/">Paloma Devi</a> - angel voice</div>
+    <div><a href="https://www.gurumukhmarkharris.com/">Gurumukh Mark Harris</a> - Mukh has been an inspiration for members of Sunheart since the 1970s!</div>
+    <div><a href="https://devapriyo.com//">Deva Priyo Nicola Notaro<a> - Priyo is featured on Sunheart's Nice, Unicorn Song and Wanderers Three</div>
   </q-page>
 </template>
 
