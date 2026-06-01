33c8aaf110a038299ff0bc0d80968bca86edc9a2
david <sundance@efn.org>
2026-05-31 17:01:06 -0700

    Fixed my typos on the Deva Priyo line of the page

--- frontend/src/pages/MoreMusicPage.vue
+++ frontend/src/pages/MoreMusicPage.vue
@@ -11,7 +11,7 @@
     <div><a href="https://tidal.com/browse/artist/5895786">Ananda Yogiji</a>- Ananda is an original Sunheart booster. We love you!</div>
     <div><a href="https://www.palomadevi.com/">Paloma Devi</a> - angel voice</div>
     <div><a href="https://www.gurumukhmarkharris.com/">Gurumukh Mark Harris</a> - Mukh has been an inspiration for members of Sunheart since the 1970s!</div>
-    <div><a href="https://devapriyo.com//">Deva Priyo Nicola Notaro<a> - Priyo is featured on Sunheart's Nice, Unicorn Song and Wanderers Three</div>
+    <div><a href="https://devapriyo.com">Deva Priyo Nicola Notaro</a> - Priyo is featured on Sunheart's Nice, Unicorn Song and Wanderers Three</div>
   </q-page>
 </template>
 
